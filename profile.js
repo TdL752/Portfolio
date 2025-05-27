d63136495e51864b32e9449174671d1becdf524a
TdL752 <tylerdamon1@aol.com>
2025-05-27 18:11:15 +0000

    Enhanced responsive features, improved sizing additions to text sections

--- profile.js
+++ profile.js
@@ -1,6 +1,8 @@
 const lightMode = document.getElementById("light");
 const darkMode = document.getElementById("dark");
 const root = document.documentElement;
+const navMenu = document.getElementById("collapsed-nav-menu");
+const navBtn = document.getElementById("nav-menu-btn");
 
 lightMode.addEventListener("click", () => {
     root.style.backgroundColor = "white";
@@ -11,3 +13,13 @@ darkMode.addEventListener("click", () => {
     root.style.backgroundColor = "black";
     root.style.color = "white";
 });
+
+navBtn.addEventListener("click", () => {
+    if(navMenu.classList.contains("active")) {
+        navMenu.classList.remove("active");
+        navMenu.classList.add("hidden");
+    } else {
+        navMenu.classList.remove("hidden");
+        navMenu.classList.add("active");
+    }
+})
